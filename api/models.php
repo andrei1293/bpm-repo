3c8f20483284f18327cef801c3057e480728277f
Andrii Kopp <kopp93@gmail.com>
2017-11-29 13:47:27 +0200

    Imlement models order by events.

--- api/models.php
+++ api/models.php
@@ -5,10 +5,13 @@ $connection = open_connection();
 
 $models = array();
 $sql = 'select process_model.Process_ID, Process_Name, Process_Model_ID,
-        Model_Type_Name, Process_Model_File_URL
+        Model_Type_Name, Process_Model_File_URL,
+        (select Event_Timestamp from event where Process_Model_ID =
+            process_model.Process_Model_ID order by Event_Timestamp desc limit 1) as Event_Timestamp
     from process_model, process, model_type
     where process_model.Process_ID = process.Process_ID and
-        process_model.Model_Type_ID = model_type.Model_Type_ID';
+        process_model.Model_Type_ID = model_type.Model_Type_ID
+    order by Event_Timestamp desc';
 
 $result = $connection->query($sql);
 
